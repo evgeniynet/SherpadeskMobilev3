c399670f24522fb1928634aa6e2ab1aaa58a19a7
Eugene <jtrue@mail.ru>
2014-05-28 17:42:01 +0300

    fixed

--- js/main.js
+++ js/main.js
@@ -12,12 +12,12 @@ $(document).ready(function(){
 					xhr.withCredentials = true;
 					xhr.setRequestHeader('Authorization', 'Basic ' + btoa('x:' + "xyjfvhjajkmcarswif5k0whm7hkhmfju"));
 					},
-				url: 'http://api.sherpadesk.com/organizations/',
+				url: 'http://api.beta.sherpadesk.com/organizations/',
 				async: true,
 				cache: false,
 				dataType: 'json',			
 				success: function(returnData) {
-					alert(returnData);
+					alert(JSON.stringify(returnData));
 				},
 				error: function() {
 					alert("fail");
