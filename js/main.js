3f73e95815df20e046ee7803b84c500f984ec275
Matthew Bennett <mbennett24@student.gsu.edu>
2014-05-28 12:21:43 -0400

    ticketCount error
    errors with the ticket count

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
-
+	
+	var userOrgKey = "";
+	var userOrg = "";
+	var	userKey = "xyjfvhjajkmcarswif5k0whm7hkhmfju";
+	
 	var org = {
 		init: function() {
 			this.getOrg();
@@ -10,24 +14,53 @@ $(document).ready(function(){
 				type: 'GET',
 				beforeSend: function (xhr) {
 					xhr.withCredentials = true;
-					xhr.setRequestHeader('Authorization', 'Basic ' + btoa('x:' + "xyjfvhjajkmcarswif5k0whm7hkhmfju"));
+					xhr.setRequestHeader('Authorization', 'Basic ' + btoa('x:' + userKey));
 					},
 				url: 'http://api.beta.sherpadesk.com/organizations/',
 				async: true,
 				cache: false,
 				dataType: 'json',			
 				success: function(returnData) {
-					alert(JSON.stringify(returnData));
+					userOrgKey = returnData[0].key;
+					userOrg = returnData[0].name;
+					$(".SherpaDesk").html(userOrg);
 				},
 				error: function() {
-					alert("fail");
+					alert("fail @ getOrg");
 				}
 			}).promise();
 		}
 	};
 
+	var ticketCount = {
+		init: function() {
+			this.getTicketCount();
+		},
+
+		getTicketCount: function() {
+			$.ajax({
+    type: 'GET',
+    beforeSend: function (xhr) {
+        xhr.setRequestHeader('Authorization', 
+                             'Basic ' + btoa(userKey));
+        },
+    url: 'http://api.beta.sherpadesk.com' + '/tickets/emmjf5',
+    data: {}, 
+    dataType: 'json',
+    success: function (d) {
+             alert('Success! Got ticket ' + d.subject);  
+    },
+    error: function (e, textStatus, errorThrown) {
+             alert(errorThrown);
+    }
+ });
+		}
+	};
+
+
 	(function() {
 		org.init();
+		ticketCount.init();
 	}()); 
 	
 
